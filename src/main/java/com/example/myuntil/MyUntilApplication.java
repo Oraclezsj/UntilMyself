bc154a5d00f66a2a2b2a636967a5362d621753d6
zsjGome <3079369786@qq.com>
2022-04-30 12:05:33 +0800

    常用的java功能

---
+++ src/main/java/com/example/myuntil/MyUntilApplication.java
@@ -0,0 +1,13 @@
+package com.example.myuntil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUntilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUntilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myuntil/MyUntilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myuntil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUntilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
